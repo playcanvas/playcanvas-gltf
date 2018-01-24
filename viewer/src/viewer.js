89bf3fa0fb23a42eef50b0b05ca2692965c06526
Will Eastcott <will@playcanvas.com>
2018-01-23 19:48:17 -0800

    Fix Electron build

--- viewer/src/viewer.js
+++ viewer/src/viewer.js
@@ -60,15 +60,15 @@ function main() {
 
     // set a prefiltered cubemap as the skybox
     var cubemapAsset = new pc.Asset('helipad', 'cubemap', {
-        url: "./cubemap/6079289/Helipad.dds"
+        url: "./assets/cubemap/6079289/Helipad.dds"
     }, {
         "textures": [
-            "./cubemap/6079292/Helipad_posx.png",
-            "./cubemap/6079290/Helipad_negx.png",
-            "./cubemap/6079293/Helipad_posy.png",
-            "./cubemap/6079298/Helipad_negy.png",
-            "./cubemap/6079294/Helipad_posz.png",
-            "./cubemap/6079300/Helipad_negz.png"
+            "./assets/cubemap/6079292/Helipad_posx.png",
+            "./assets/cubemap/6079290/Helipad_negx.png",
+            "./assets/cubemap/6079293/Helipad_posy.png",
+            "./assets/cubemap/6079298/Helipad_negy.png",
+            "./assets/cubemap/6079294/Helipad_posz.png",
+            "./assets/cubemap/6079300/Helipad_negz.png"
         ],
         "magFilter": 1,
         "minFilter": 5,
