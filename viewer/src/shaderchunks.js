debd74cca5ea56edb798de5291064974e60033fd
kungfooman <lama12345@gmail.com>
2018-10-29 03:45:59 +0100

    add timeline.js to separate Timeline from viewer.js and shaderchunks.js fixes

--- viewer/src/shaderchunks.js
+++ viewer/src/shaderchunks.js
@@ -2,18 +2,30 @@ ShaderChunks = function() {
     this.shaderchunks = document.getElementById("shaderchunks");
     this.htmlShaderChunks = {};
     for (var name in pc.shaderChunks) {
+        var shaderChunk = pc.shaderChunks[name];
+        if (typeof shaderChunk !== "string") // there are a few functions
+            continue;
         var text = document.createElement("div");
         var textarea = document.createElement("textarea");
         text.innerHTML = name;
-        textarea.value = pc.shaderChunks[name];
+        var numNewlines = 1;
+        for (var i=0; i<shaderChunk.length; i++)
+            if (shaderChunk[i] == "\n")
+                numNewlines++;
+        textarea.value = shaderChunk;
+        textarea.style.height = (numNewlines * 16) + "px";
         this.htmlShaderChunks[name] = textarea;
         this.shaderchunks.appendChild(text);
         this.shaderchunks.appendChild(textarea);
     }
+    this.shaderchunks.style.position = "absolute";
+    this.shaderchunks.style.left = "0px";
+    this.shaderchunks.style.top = "40px";
     this.shaderchunks.style.overflow = "scroll";
     this.resize();
 }
 
 ShaderChunks.prototype.resize = function() {
-    this.shaderchunks.style.height = window.innerHeight + "px";
+    this.shaderchunks.style.width = window.innerWidth + "px";
+    this.shaderchunks.style.height = (window.innerHeight - 40) + "px";
 }
