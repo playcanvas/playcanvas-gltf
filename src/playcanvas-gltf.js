ce320afd1b5bda11815fedd56c2add91391d8036
aidinabedi <aidinabedi@gmail.com>
2019-07-15 23:23:52 +0200

    remove use of dDiffuseLight in combineColor to ensure the color is fully unlit

--- src/playcanvas-gltf.js
+++ src/playcanvas-gltf.js
@@ -385,6 +385,24 @@
 
         if (data.hasOwnProperty('extensions') && data.extensions.hasOwnProperty('KHR_materials_unlit')) {
             material.useLighting = false;
+
+            // Make sure diffuse is not actually combined with any form of lighting
+            material.chunks.combineDiffusePS = [
+                "vec3 combineColor() {",
+                "    return dAlbedo;",
+                "}",
+            ].join('\n') + "\n";
+
+            material.useSpecular = false;
+
+            // NOTE: Although useSpecular is a documented property of pc.StandardMaterial,
+            // it doesn't seem to be used. Hence why below chunks need to be changed as well: 
+            material.chunks.combineDiffuseSpecularPS = 
+                material.chunks.combineDiffuseSpecularOldPS = 
+                material.chunks.combineDiffuseSpecularNoReflPS = 
+                material.chunks.combineDiffuseSpecularNoConservePS = 
+                material.chunks.combineDiffuseSpecularNoReflSeparateAmbientPS =
+                material.chunks.combineDiffusePS;
         }
 
         var color, texture;
